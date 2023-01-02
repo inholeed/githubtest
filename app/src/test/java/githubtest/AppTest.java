8d09baf33f44f46dce053d1556524e6a032ed794
example <example@example.com>
2023-01-02 17:08:25 +0900

    remove fail

--- app/src/test/java/githubtest/AppTest.java
+++ app/src/test/java/githubtest/AppTest.java
@@ -11,7 +11,4 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
-    @Test void failTest() {
-	assertNotNull(null, "this should fail");
-    }
 }
